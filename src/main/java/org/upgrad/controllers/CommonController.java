78821a3e978f8ddbf7156d89c62b0d35264f7e3a
joeljoy <joelvjoy94@gmail.com>
2018-07-15 19:57:48 +0530

    added common controller

--- src/main/java/org/upgrad/controllers/CommonController.java
+++ src/main/java/org/upgrad/controllers/CommonController.java
@@ -1,8 +1,30 @@
 package org.upgrad.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.upgrad.services.CategoryService;
 
 @Controller
 public class CommonController {
+
+	@Autowired
+	CategoryService categoryService;
+
+	@GetMapping("/api/categories/all")
+	public ResponseEntity<?> getAllCategories() {
+		ResponseEntity<?> categoryList = new ResponseEntity<>(categoryService.getAllCategories(), HttpStatus.OK);
+
+		return categoryList;
+	}
+
+	@GetMapping("/api/questions/all")
+	public ResponseEntity<?> getAllQuestions() {
+		ResponseEntity<?> questionsList = new ResponseEntity<>("All Questions", HttpStatus.OK);
+
+		return questionsList;
+	}
 
 }
